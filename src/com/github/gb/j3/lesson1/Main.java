f32e1208add7e333cc90451b9a79d65762bdd7db
rmorozov <rmorozov@hse.ru>
2020-02-19 12:31:55 +0300

    N1

--- src/com/github/gb/j3/lesson1/Main.java
+++ src/com/github/gb/j3/lesson1/Main.java
@@ -1,7 +1,21 @@
 package com.github.gb.j3.lesson1;
 
+import com.github.gb.j3.lesson1.swap.Swap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("first commit");
+        System.out.println("Swap:");
+        String[] arr = new String[]{"str1", "str2", "str3"};
+        print(arr);
+        Swap.process(arr, 0, 2);
+        print(arr);
+    }
+
+    private static <T> void print(T[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if(i != arr.length - 1) System.out.print(", ");
+        }
+        System.out.println();
     }
 }
